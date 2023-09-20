8a61d30d4a0f278ca6cdbf1a8cb69cefb730de72
Rentsen <rentsena1@gmail.com>
2023-09-20 12:25:54 +0800

    setup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,37 @@
 import express from "express"
 
-
+import dotenv from "dotenv"
+import morgan from "morgan";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors"
+dotenv.config()
 
 const app =express();
+// Morgan
+if(process.env.NODE_ENV !=="production")
+{
+    app.use(morgan("dev"))
+}
+// Helmet
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(ExpressMongoSanitize())
+app.use(cookieParser())
+app.use(compression())
+app.use(fileUpload({
+    useTempFiles:true
+}))
+app.use(cors({origin:"http://localhost:3000"}))
 app.get('/',(req,res)=>{
     res.send("Helloo from server")
 })
+app.post('/test2',(req,res)=>{
+   res.send(req.body.name)
+}
+)
 export default app
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import app from "./app.js"
-import dotenv from "dotenv"
-dotenv.config()
+
 const PORT =process.env.PORT || 8000
 app.listen(PORT,()=>{
     console.log(`Server is starting1 ${PORT}`);
