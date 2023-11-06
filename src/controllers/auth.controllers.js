a83779e76833504282d6a82b6f5024f9115fcd68
rentsen <rentsena1@gmail.com>
2023-11-06 15:46:52 +0800

    ss

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,14 +1,18 @@
 import { createUser, signUser } from "../services/auth.services.js";
 let Access_token="M8L1tJzIpUHhLRgvOXVWu7";
 import { generateToken } from "../services/token.service.js";
+import { verify } from "../utils/token.utils.js";
 let refresh_token="THE5i6XYF8gytYy6pbgSWC"
+// let access_tokenAuth="THE5i6XYF8gytYy6pbgSWC"
+import { findUser } from "../services/user.sevices.js";
 export const register =async(req,res,next)=>{
  try{
     const {name,email,picture,status,password}=req.body;
+    console.log("aa",name)
     const newUser = await createUser({
       name,email,picture,status,password
     })
-    console.log(Access_token)
+   //  console.log(Access_token)
     const access_token=await generateToken({userId:newUser._id},"1d",Access_token)
     const ref_token=await generateToken({userId:newUser._id},"30d",refresh_token)
     res.cookie("refreshtoken",ref_token,{
@@ -17,7 +21,7 @@ export const register =async(req,res,next)=>{
       maxAge:30*24*60*60*1000
     })
     console.table({ref_token,access_token})
-    res.status(200).json({message:"success",data:newUser,access_token})
+    res.status(200).json({message:"success",data:{...newUser,access_token}})
  }  catch (error){
     next(error)
 
@@ -34,9 +38,47 @@ export const login =async(req,res,next)=>{
         path:'/api/v1/auth/refreshtoken',
         maxAge:30*24*60*60*1000
       })
-      res.status(200).json({message:"success",data:user,access_token})
+      res.status(200).json({message:"success",data:{...user,access_token}})
    }catch(error){
       next(error)
    }
  
 }
+export const logout =async()=>{
+   try{
+// res.clearCookie('refreshtoken',{path:"api/v1/auth/refreshtoken"})
+res.status(200).json({message:"log out"})
+   }catch(error){
+
+   }
+}
+export const refreshToken = async (req, res, next) => {
+   try {
+     const refresh_tokens = req.cookies.refreshtoken;
+     console.log("ref",refresh_tokens)
+     if (!refresh_tokens) throw createHttpError.Unauthorized("Please login.");
+   
+     const check = await verify(
+       refresh_tokens,
+       refresh_token
+     );
+     const user = await findUser(check.userId);
+     const access_token = await generateToken(
+       { userId: user._id },
+       "1d",
+       Access_token
+     );
+     res.json({
+       user: {
+         _id: user._id,
+         name: user.name,
+         email: user.email,
+         picture: user.picture,
+         status: user.status,
+         token: access_token,
+       },
+     });
+   } catch (error) {
+     next(error);
+   }
+ };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,7 +1,10 @@
 import express from "express"
-import {login, register} from "../controllers/auth.controllers.js"
+// import trimRequest from "trim"
+import {login, register,logout,refreshToken} from "../controllers/auth.controllers.js"
 const router = express.Router();
 
 router.route("/register").post(register)
 router.route("/login").post(login)
+router.route("/refreshtoken").post(refreshToken)
+router.route("/logout").post(logout)
 export default router  
